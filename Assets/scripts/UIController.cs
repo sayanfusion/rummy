b73f47527598836c78e4dde6466545666dca0bf0
AlekhyaGameDev <fusiontechlab386@gmail.com>
2025-08-07 13:05:45 +0530

    Scene Updated

--- Assets/scripts/UIController.cs
+++ Assets/scripts/UIController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
@@ -33,6 +34,11 @@ public class UIController : MonoBehaviour
             currActivePanel = null;
         }
 
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("mainMenu");
     }
 }
 
